00538922efcf8ded0a341e42900ca522705c62ee
Marvin Caharop <Lebbraumjayce@gmail.com>
2023-08-17 11:32:51 +0800

    keep old data

--- resources/views/pages/job-create.blade.php
+++ resources/views/pages/job-create.blade.php
@@ -20,8 +20,9 @@
         <div class="form-container">
         <div class="mb-6">
           <label for="company" class="inline-block text-lg mb-2">Company Name</label>
+          {{-- keep old data --}}
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
-            value="{{old('company')}}" />
+            value="{{old('company')}}" /> 
   
           @error('company')
           <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -41,7 +42,7 @@
         <div class="mb-6">
           <label for="location" class="inline-block text-lg mb-2">Job Location</label>
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
-            placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}" />
+            placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}" /> {{-- keep old data --}}
   
           @error('location')
           <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -52,6 +53,7 @@
           <label for="email" class="inline-block text-lg mb-2">
             Contact Email
           </label>
+          {{-- keep old data --}}
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}" />
   
           @error('email')
@@ -64,7 +66,7 @@
             Website/Application URL
           </label>
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website"
-            value="{{old('website')}}" />
+            value="{{old('website')}}" /> {{-- keep old data --}}
   
           @error('website')
           <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -76,7 +78,7 @@
             Tags (Comma Separated)
           </label>
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
-            placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}" />
+            placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}" />{{-- keep old data --}}
   
           @error('tags')
           <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -99,7 +101,7 @@
             Job Description
           </label>
           <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
-            placeholder="Include tasks, requirements, salary, etc">{{old('description')}}</textarea>
+            placeholder="Include tasks, requirements, salary, etc">{{old('description')}}</textarea>{{-- keep old data --}}
   
           @error('description')
           <p class="text-red-500 text-xs mt-1">{{$message}}</p>
