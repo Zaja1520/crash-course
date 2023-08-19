d58906003e35024da0493c6416d037cf683931ad
Marvin Caharop <Lebbraumjayce@gmail.com>
2023-08-19 10:16:15 +0800

    brief summary

---
+++ notes.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+
+setup 
+    xampp
+    laravel
+    vs code
+    virtual host
+    configure environment
+
+
+
+routing
+    create route for controller
+    routes can accept parameters
+
+
+
+use of dd()?
+    display the variable's contents and prevent further script execution
+
+
+
+api routes?
+    a route for api seperated on web routes
+    typically return JSON responses
+
+
+
+passing a data to blade on laravel
+    return view('page', ['key' => 'value]);
+
+
+
+model creation
+    php artisan make:model name
+
+
+
+migration file
+    the tables of the models
+
+template partials?
+    the templates to be included in the template
+
+
+
+status responses
+200, 201,
+401, 404,
+
+
+
+blade components?
+ stored in views/components/
+ to call the component on other page used <x-componentname />
+ can also pass parameters on it
+
+
+
+search filter?
+   define the query inside the model of to seach for
+   $query->where your query
+
+validation common used
+    to validate the submitted data before do something with it
+
+
+
+flash messages
+    used to display message one time
+    this was passed by controller to templates
+
+
+
+relationships
+    one to one
+    one to many
+    many to many
+*/
